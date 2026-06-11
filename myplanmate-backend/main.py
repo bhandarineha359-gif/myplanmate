f96f9952766f65bee1009f533ef4626a7b508dd0
bhandarineha359-gif <bhandarineha359@gmail.com>
2026-06-11 09:51:37 +0100

    Update main.py

--- myplanmate-backend/main.py
+++ myplanmate-backend/main.py
@@ -53,8 +53,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[FRONTEND_URL, "http://localhost:5173"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
